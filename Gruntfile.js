819c72e221b22209bad65eda7859268279340838
root <root@localhost.localdomain>
2018-09-22 09:49:24 +0530

    Separate folder created for keeping Bootstrap & Jquery in public

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,33 @@ module.exports = function (grunt) {
 
     require('load-grunt-tasks')(grunt);
 
-    var config = {        
+    var config = {
+        mkdir: {
+            tmp: {
+                options: {
+                    create: ['tmp']
+                }
+            }
+        },
+        gitclone: {
+            hterm: {
+                options: {
+                    cwd: './tmp',
+                    repository: 'https://chromium.googlesource.com/apps/libapps'
+                }
+            }
+        },
+        shell: {
+            build_hterm: {
+                command: 'LIBDOT_SEARCH_PATH=$(pwd) ./libdot/bin/concat.sh -i ./hterm/concat/hterm_all.concat -o ../../public/wetty/hterm_all.js',
+                options: {
+                    execOptions: {
+                        cwd: './tmp/libapps'
+                    }
+                }
+            }
+        },
+        clean: ['./tmp'],
         copy: {
             js: {
                 files: [
@@ -10,7 +36,13 @@ module.exports = function (grunt) {
                         expand: true,
                         flatten: true,
                         src: ['node_modules/bootstrap/dist/js/*'],
-                        dest: 'public/wetty/js/' 
+                        dest: 'public/wetty/bootstrap/js/' 
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['node_modules/jquery/dist/*'],
+                        dest: 'public/wetty/jquery/' 
                     }
                 ]
             },
@@ -20,7 +52,7 @@ module.exports = function (grunt) {
                         expand: true,
                         flatten: true,
                         src: ['node_modules/bootstrap/dist/css/*'],
-                        dest: 'public/wetty/css/' 
+                        dest: 'public/wetty/bootstrap/css/'
                     }
                 ]
             }
@@ -28,8 +60,8 @@ module.exports = function (grunt) {
     };
 
     grunt.initConfig(config);
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('default', ['copy:js', 'copy:css']);
 
-    
+    grunt.registerTask('update-hterm', ['mkdir:tmp', 'gitclone:hterm', 'shell:build_hterm', 'clean']);
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
 };
